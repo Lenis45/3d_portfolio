091eef22d78bc57eff7c0db12ab6b44f938edbb6
Denis Kolesnikov <116916114+Lenis45@users.noreply.github.com>
2023-07-02 15:01:43 +0300

    fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,13 +9,12 @@ const Hero = () => {
     <section className={`relative w-full h-screen mx-auto`}>
       <div
       // линия с левой стороны 
-        className={`absolute inset-0 top-[120px] max-w-7x1 mx-auto ${styles.paddingX} 
-        flex flex-row items-start gap-5`}>
-
-          <div className="flex flex-col justify-center items-center mt-5">
-            <div className="w-5 h-5 rounded-full bg-[#915EFF]"/>
-            <div className="w-1 sm:h-80 h-40 violet-gradient"/>
-          </div>
+      className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+      >
+        <div className='flex flex-col justify-center items-center mt-5'>
+          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
+          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+        </div>
           {/* создаём текст о себе */}
           <div> 
             <h1 className={`${styles.heroHeadText} text-white`}>
@@ -30,24 +29,26 @@ const Hero = () => {
 
       <ComputersCanvas />
       {/* закрепляем ссылку на о мне */}
-        <div className="absolute xs:botton-10 botton-32 w-full justify-center items-center">
-            <a href="#about"> 
-              <div className="w-[35px] h-[64px] rouded-3x1 border-4 border-secondary flex justify-center items-start p-2">
-                <motion.div
-                  animate={{y: [0, 24, 0],}}
-                  transition={{
-                    duration: 1.5,
-                    repeat: Infinity,
-                    repeatType: "loop",
-                  }}
-                  className="w-3 h-3 rounded-full bg-secondary mb-1"
-                  />
-              </div>
-            </a>
-        </div>
+      {/* создаём значёк мышки  */}
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href='#about'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
+            />
+          </div>
+        </a>
+      </div>
     </section>
-    
-  )
-}
+  );
+};
 
 export default Hero
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,23 +4,23 @@ const CanvasLoader = () => {
   const { progress } = useProgress();
   return (
     <Html
-    as='div'
-    center
-    style={{
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      flexDirection: 'column',
-    }}
+      as='div'
+      center
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
     >
-      <span className="canvas-loader"></span>
-      <p 
+      <span className='canvas-loader'></span>
+      <p
         style={{
           fontSize: 14,
-          color: '#F1F1F1',
+          color: "#F1F1F1",
           fontWeight: 800,
           marginTop: 40,
-        }}  
+        }}
       >
         {progress.toFixed(2)}%
       </p>
