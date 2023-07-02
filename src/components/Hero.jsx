3330e379abc24346e8ccd8bdea7349ede44f09be
Denis Kolesnikov <116916114+Lenis45@users.noreply.github.com>
2023-07-02 14:40:34 +0300

    creating Computers and Hero components

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import { motion } from "framer-motion";
 
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
+
+// создаём секцию для главной станицы
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className={`relative w-full h-screen mx-auto`}>
+      <div
+      // линия с левой стороны 
+        className={`absolute inset-0 top-[120px] max-w-7x1 mx-auto ${styles.paddingX} 
+        flex flex-row items-start gap-5`}>
+
+          <div className="flex flex-col justify-center items-center mt-5">
+            <div className="w-5 h-5 rounded-full bg-[#915EFF]"/>
+            <div className="w-1 sm:h-80 h-40 violet-gradient"/>
+          </div>
+          {/* создаём текст о себе */}
+          <div> 
+            <h1 className={`${styles.heroHeadText} text-white`}>
+            Меня завут <span className="text-[#915EFF]"> Андрей </span> 
+            </h1>
+            <p className={`${styles.heroSubText} mt-2 text-white-100`}> 
+            Я разрабатываю 3D-визуализацию, пользовательские  <br className='sm:block hidden' />
+            интерфейсы и веб-приложения
+            </p>
+          </div>
+        </div>
+
+      <ComputersCanvas />
+      {/* закрепляем ссылку на о мне */}
+        <div className="absolute xs:botton-10 botton-32 w-full justify-center items-center">
+            <a href="#about"> 
+              <div className="w-[35px] h-[64px] rouded-3x1 border-4 border-secondary flex justify-center items-start p-2">
+                <motion.div
+                  animate={{y: [0, 24, 0],}}
+                  transition={{
+                    duration: 1.5,
+                    repeat: Infinity,
+                    repeatType: "loop",
+                  }}
+                  className="w-3 h-3 rounded-full bg-secondary mb-1"
+                  />
+              </div>
+            </a>
+        </div>
+    </section>
+    
   )
 }
 
